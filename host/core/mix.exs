ea930d916eed6262407c6af74b1a5b5f9a53b139
Tobias Bohwalli <hi@futhr.io>
2026-09-22 15:10:53 +0200

    chore: use current stable language toolchains

--- host/core/mix.exs
+++ host/core/mix.exs
@@ -5,7 +5,7 @@ defmodule FrameshiftCore.MixProject do
     [
       app: :frameshift_core,
       version: "0.1.0-dev",
-      elixir: "~> 1.19",
+      elixir: "~> 1.20 and >= 1.20.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
@@ -23,7 +23,8 @@ defmodule FrameshiftCore.MixProject do
 
   defp deps do
     [
-      {:jsv, "~> 0.22.0"}
+      {:jsv, "~> 0.22.0"},
+      {:rfc8785, "~> 1.0.0"}
     ]
   end
 
